0cdf001095ce28697616a01effca063d19cfe634
eodaneze <ezealiokechukwu1999@gmail.com>
2023-10-21 01:27:32 -0700

    setting the mockup of the header

--- app/index.js
+++ app/index.js
@@ -1,10 +1,49 @@
-import { View, Text } from "react-native";
-  const Home = () => {
-    return (
-          <View>
-            <Text>Home</Text>
-          </View>
-        );
-  }
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import {Stack, useRouter } from "expo-router";
 
-  export default Home;
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  Populations,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+const Home = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: COLORS.lightWhite }}
+      >
+      
+      <Stack.Screen 
+        options={{
+          headerStyle: {backgroundColor: COLORS.lightWhite},
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%'/>
+           ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension='100%'/>
+           ),
+           headerTitle: ""
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={{
+            flex: 1,
+            padding: SIZES.medium
+          }}>
+             <Welcome />
+
+             <Popularjobs />
+             <Nearbyjobs />
+          </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;

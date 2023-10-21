aa1f9e5231ea08cdca4a9fe5c864a0bcf20dfd94
eodaneze <ezealiokechukwu1999@gmail.com>
2023-10-21 02:38:16 -0700

    designing the job type card and starting the popular job card section

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
+import React,{useState} from 'react'
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
 import styles from './popularjobs.style'
+import { useRouter } from 'expo-router'
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from '../../common/cards/popular/PopularJobCard'
 
 const Popularjobs = () => {
+  const router = useRouter();
+  const isLoading = false;
+  const error = false;
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View styles={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+         <TouchableOpacity>
+            <Text style={styles.headerBtn}>Show All</Text>
+         </TouchableOpacity>
+      </View>
+      <View style={styles.cardsContainer}>
+          
+           {isLoading ? (
+              <ActivityIndicator size='large' colors={COLORS.primary}/>
+           ) : error ? (
+              <Text>Something went wrong</Text>
+           ) : (
+              <FlatList 
+                 data={[1,2,3,4]}
+                 renderItem={({item}) => (
+                   <PopularJobCard item={item}/>
+                  )}
+              />
+           )}
+      </View>
     </View>
   )
 }
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -13,6 +13,7 @@ import { icons, SIZES } from "../../../constants";
 const jobTypes = ['Full-time', "Part-time", "Contractor"]
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState('Full-time')
   return (
     <View>
       <View style={styles.container}>
@@ -36,10 +37,16 @@ const Welcome = () => {
            <FlatList 
              data={jobTypes}
              renderItem={({item}) => (
-                <TouchableOpacity>
-                   <Text>{item}</Text>
+                <TouchableOpacity style={styles.tab(activeJobType, item)} onPress={() => {
+                   setActiveJobType(item);
+                   router.push(`/search/${item}`)
+                }}>
+                   <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
                 </TouchableOpacity>
               )}
+              keyExtractor={item => item}
+              contentContainerStyle={{columnGap: SIZES.small}}
+              horizontal
            />
       </View>
     </View>

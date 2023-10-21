334e87c4ddd9ba3630d6b96c4c02eb59d3df0ebf
eodaneze <ezealiokechukwu1999@gmail.com>
2023-10-21 02:11:12 -0700

    understanding a flatlist

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -10,6 +10,7 @@ import {
 import { useRouter } from "expo-router";
 import styles from "./welcome.style";
 import { icons, SIZES } from "../../../constants";
+const jobTypes = ['Full-time', "Part-time", "Contractor"]
 const Welcome = () => {
   const router = useRouter();
   return (
@@ -32,7 +33,14 @@ const Welcome = () => {
         </TouchableOpacity>
       </View>
       <View style={styles.tabsContainer}>
-           <FlatList />
+           <FlatList 
+             data={jobTypes}
+             renderItem={({item}) => (
+                <TouchableOpacity>
+                   <Text>{item}</Text>
+                </TouchableOpacity>
+              )}
+           />
       </View>
     </View>
   );
